9110eeb158ca2e4e0ca888fe7f68ba130fbd5f53
HidetoHigashi <hideto.higashi@glpgs.com>
2023-03-13 09:10:36 +0900

    不要なテスト削除

--- SampleProjectTests/SampleProjectTests.swift
+++ SampleProjectTests/SampleProjectTests.swift
@@ -12,19 +12,4 @@ import ComposableArchitecture
 
 @MainActor
 final class SampleProjectTests: XCTestCase {
-    typealias State = WeatherStore.State
-    typealias Reducer = WeatherStore
-
-    func testAreaAPI() async throws {
-        let store = TestStore(initialState: State(), reducer: Reducer())
-        let data = try await store.dependencies.weatherClient.fetchAreaList()
-        print(type(of: data.offices))
-    }
-
-    func testWeatherAPI() async throws {
-        let store = TestStore(initialState: State(), reducer: Reducer())
-        store.exhaustivity = .off
-        let data = try await store.dependencies.weatherClient.fetchWeatherInfomation("130000")
-        print(data)
-    }
 }
